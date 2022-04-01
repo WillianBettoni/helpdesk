808d0cebb98d8646c817d9c8eb8d8759d302f5c0
willianbettoni <willian.willalex@gmail.com>
2022-03-31 22:24:40 -0300

    Aula Java

--- src/main/java/com/willian/helpdesk/domain/Cliente.java
+++ src/main/java/com/willian/helpdesk/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.willian.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.willian.helpdesk.domain.dtos.ClienteDTO;
 import com.willian.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -26,6 +28,17 @@ public class Cliente extends Pessoa {
 	public Cliente(Integer id, String nome, String cpf, String senha, String email) {
 		super(id, nome, cpf, senha, email);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.senha = obj.getSenha();
+		this.email = obj.getEmail();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
